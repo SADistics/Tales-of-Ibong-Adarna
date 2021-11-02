a14d59f66d69f83877d2fb9d919e330573aa4350
Christian Paul L. Bustamante <christianpaulbustamante49@gmail.com>
2021-11-02 11:41:21 +0800

    - Interaction Prompt Added

--- Assets/PlayerInteractDialogue.cs
+++ Assets/PlayerInteractDialogue.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteractDialogue : MonoBehaviour
 {
@@ -11,10 +12,19 @@ public class PlayerInteractDialogue : MonoBehaviour
 
     public VIDE_Assign inTrigger;
 
+    private Text interactionPromptText;
+    private GameObject interactionPrompt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        interactionPrompt = GameObject.Find("InteractionPrompt");
+        if (interactionPrompt != null)
+        {
+            interactionPrompt.SetActive(false);
+        }
+        interactionPromptText = GameObject.Find("InteractionPromptText").GetComponent<Text>();
+        interactionPromptText.text = "";
     }
 
     // Update is called once per frame
@@ -23,6 +33,7 @@ public class PlayerInteractDialogue : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             TryInteract();
+            interactionPrompt.SetActive(false);
         }
     }
 
@@ -37,14 +48,19 @@ public class PlayerInteractDialogue : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        interactionPrompt.SetActive(true);
         if (other.GetComponent<VIDE_Assign>() != null)
         {
             inTrigger = other.GetComponent<VIDE_Assign>();
+            interactionPromptText.text = "Press 'E' to interact with "+inTrigger.alias;
         }
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         inTrigger = null;
+        interactionPrompt.SetActive(false);
+        interactionPromptText.text = "";
     }
 }
